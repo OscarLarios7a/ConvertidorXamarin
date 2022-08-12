6aaa7f370231b99eb93c16c2baf847e1bc049c19
Oscar Larios <javier.atoaxaca@gmail.com>
2022-08-12 17:23:59 -0500

    Cargando Proyecto App Xamarin

---
+++ App2/Vistas/Convertir.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App2.Vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Convertir : ContentPage
+    {
+        double cm;
+        double m;
+        public Convertir()
+        {
+            InitializeComponent();
+        }
+
+        private void Calcular()
+        {
+            cm = Convert.ToDouble(txtDatos.Text);
+            m = cm/100;
+            txtResultado.Text = m.ToString() + " m";
+        }
+        private void Validar()
+        {
+            if (!string.IsNullOrEmpty(txtDatos.Text))
+            {
+                Calcular();
+            }
+            else
+            {
+                DisplayAlert("Error", "Ingrese un Cantidad", "OK");
+            }
+        }
+
+        private void btnVolver_Clicked(object sender, EventArgs e)
+        {
+           Navigation.PopAsync();
+        }
+
+        private void btnCalcular_Clicked(object sender, EventArgs e)
+        {
+            Validar();
+        }
+    }
+}
